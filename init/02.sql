ad0e5baa4b0c89e28e48339cfc356982139bac5f
NPLeeWenKang <s10203100@connect.np.edu.sg>
2022-11-30 01:29:20 +0800

    fix name

--- init/02.sql
+++ init/02.sql
@@ -7,8 +7,8 @@ INSERT INTO passanger(passanger_id, first_name, last_name, mobile_no, email) VAL
 (5, "Zane", "Chua", 11111114, "zane@gmail.com");
 
 INSERT INTO driver(driver_id, first_name, last_name, mobile_no, email, id_no, car_no) VALUES
-(1, "Diver 1", "Lee", 88888880, "Diver1@gmail.com", "id1111", "car1111"),
-(2, "Diver 2", "Lim", 88888881, "Diver2@gmail.com", "id2222", "car2222"),
-(3, "Diver 3", "Chua", 88888882, "Diver3@gmail.com", "id3333", "car3333"),
-(4, "Diver 4", "Ang", 88888883, "Diver4@gmail.com", "id4444", "car4444"),
-(5, "Diver 5", "Chua", 88888884, "Diver5@gmail.com", "id55555", "car5555");
+(1, "Driver 1", "Lee", 88888880, "Diver1@gmail.com", "id1111", "car1111"),
+(2, "Driver 2", "Lim", 88888881, "Diver2@gmail.com", "id2222", "car2222"),
+(3, "Driver 3", "Chua", 88888882, "Diver3@gmail.com", "id3333", "car3333"),
+(4, "Driver 4", "Ang", 88888883, "Diver4@gmail.com", "id4444", "car4444"),
+(5, "Driver 5", "Chua", 88888884, "Diver5@gmail.com", "id55555", "car5555");
